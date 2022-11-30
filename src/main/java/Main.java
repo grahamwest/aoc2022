8be31a74bc63facd76c362a9fba8966fda1b0a58
Graham West <grahamwest@grahams-mbp.lan>
2022-11-30 23:18:40 +0000

    Initial template for AoC 2022 with Java Manifold

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.grahamwest.aoc2022.AdventOfCode;
+
+public class Main {
+    public static void main(String[] args) {
+        AdventOfCode aoc = new AdventOfCode();
+
+        System.out.println(aoc.dec1());
+    }
+}
---
+++ src/main/java/org/grahamwest/aoc2022/AdventOfCode.java
@@ -0,0 +1,12 @@
+package org.grahamwest.aoc2022;
+
+import manifold.ext.rt.api.auto;
+import java.time.Month;
+
+public class AdventOfCode {
+
+    public auto dec1() {
+        return (month: Month.DECEMBER, day: 1);
+    }
+
+}
